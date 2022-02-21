d96983d90989d88c174b1515b989e9c413b65f5a
Ackerman <1019226202@qq.com>
2022-02-22 01:14:28 +0800

    add test scene

---
+++ Assets/Scripts/Test/TestAssetBundle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestAssetBundle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Load());
+    }
+    IEnumerator Load() {
+        string path = Path.Combine(Application.streamingAssetsPath, "AssetbundleTest", "AssetBundles");
+        WWW www = new WWW(path);
+        yield return www;
+        if (www.isDone)
+        {
+            AssetBundle ab = www.assetBundle;
+            AssetBundleManifest assetBundleManifest = ab.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+            Debug.Log(assetBundleManifest.GetAllAssetBundles().Length);
+            foreach (string str in assetBundleManifest.GetAllAssetBundles()) {
+                print(str+"**********************************************");
+                foreach (string depency in assetBundleManifest.GetAllDependencies(str)) {
+                    print(str+" Depency:" + depency);
+                }
+            }
+        }
+        else {
+            Debug.LogError(www.error);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
